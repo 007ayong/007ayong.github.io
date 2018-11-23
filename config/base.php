d047c3aad5722088280262e176ef0b5c7aac28d6
007ayong <42128737+007ayong@users.noreply.github.com>
2018-11-23 16:16:47 +0800

    Add files via upload

--- config/base.php
+++ config/base.php
@@ -1,1 +1,10 @@
-
+<？php 返回数组（  
+  ' client_id '  =>  ' 4748b55b-210b-48b5-8f5d-8f08ddc16a0d '，
+  ' client_secret '  =>  ' kzqzoUZFL6121?gxURV1#{: '，
+  ' redirect_uri '  =>  ' https://ju.tn/onedrive-login '，
+  ' onedrive_root ' =>  ' '， // onedrive中要共享的目录，默认为根目录，可以改成如：/ share
+  ' cache_type ' =>  ' file '， // sqlite需要pdo支持
+  ' cache_expire_time '  =>  3600，
+  ' cache_refresh_time '  =>  600，
+  ' root_path '  =>  '？“
+）;
