c67e76cf062d3b65c4de0ac68c95addcd49d7af2
007ayong <42128737+007ayong@users.noreply.github.com>
2018-12-19 15:54:55 +0800

    Add files via upload

---
+++ config/base.php
@@ -0,0 +1,104 @@
+<?php return array (
+  'site_name' => 'OneIndex',
+  'password' => 'oneindex',
+  'style' => 'nexmoe',
+  'onedrive_root' => '/',
+  'cache_type' => 'secache',
+  'cache_expire_time' => 3600,
+  'cache_refresh_time' => 600,
+  'root_path' => '?',
+  'show' => 
+  array (
+    'stream' => 
+    array (
+      0 => 'txt',
+    ),
+    'image' => 
+    array (
+      0 => 'bmp',
+      1 => 'jpg',
+      2 => 'jpeg',
+      3 => 'png',
+      4 => 'gif',
+    ),
+    'video5' => 
+    array (
+      0 => 'mp4',
+      1 => 'webm',
+      2 => 'mkv',
+    ),
+    'video' => 
+    array (
+    ),
+    'video2' => 
+    array (
+      0 => 'avi',
+      1 => 'mpg',
+      2 => 'mpeg',
+      3 => 'rm',
+      4 => 'rmvb',
+      5 => 'mov',
+      6 => 'wmv',
+      7 => 'asf',
+      8 => 'ts',
+      9 => 'flv',
+    ),
+    'audio' => 
+    array (
+      0 => 'ogg',
+      1 => 'mp3',
+      2 => 'wav',
+    ),
+    'code' => 
+    array (
+      0 => 'html',
+      1 => 'htm',
+      2 => 'php',
+      3 => 'css',
+      4 => 'go',
+      5 => 'java',
+      6 => 'js',
+      7 => 'json',
+      8 => 'txt',
+      9 => 'sh',
+      10 => 'md',
+    ),
+    'doc' => 
+    array (
+      0 => 'csv',
+      1 => 'doc',
+      2 => 'docx',
+      3 => 'odp',
+      4 => 'ods',
+      5 => 'odt',
+      6 => 'pot',
+      7 => 'potm',
+      8 => 'potx',
+      9 => 'pps',
+      10 => 'ppsx',
+      11 => 'ppsxm',
+      12 => 'ppt',
+      13 => 'pptm',
+      14 => 'pptx',
+      15 => 'rtf',
+      16 => 'xls',
+      17 => 'xlsx',
+    ),
+  ),
+  'images' => 
+  array (
+    'home' => false,
+    'public' => false,
+    'exts' => 
+    array (
+      0 => 'jpg',
+      1 => 'png',
+      2 => 'gif',
+      3 => 'bmp',
+    ),
+  ),
+  'client_secret' => 'afbcoHKLGDP19271wbM?}[$',
+  'client_id' => '6cb623d0-5950-46e3-b25d-dc770bd3547d',
+  'redirect_uri' => 'https://ju.tn/',
+  'refresh_token' => 'OAQABAAAAAAC5una0EUFgTIF8ElaxtWjTLkrKjAoP4nlERbll7KgZtIFba7NXjHbhjYCjr6EJf4IpS8yItijnKU6GZ203sDFWaCXWStZyCoDf7GKi68lMi0bnwfKOresIipYfGFezPYv_A0L_dtJv1nNHZzOWlGA91W4yd3mnlqaEW8SE7jK7ipMTsI_lgEYdkoH_f7_79tVLQOfsryNIgyX0rWfrozxWpAj8UGxrWRE-feG8pGG_xIv6cfQvC4xMsNwK-ZhApW1zAm_E_4Y7cNiayZAJM73-ej-PSSYc0xTNNroCWAkEVdc0404Li7jA4EUPszepaviEZUXi_vxGhQG8ObMmldRDf51rk2BdTVXru2Dd4EL4R6dZxMIRCM6QdLUAdjpRVd_LhRiwNhtVF5dJ15eo6sxM3Tw_gGyh3qwFDbZex6DLvXL6dpjd1POg1VzBVFUAIF6jS2cHuYxoq0qOQt3_L1mR7BBBx9Z4GdA1KKyb2L8qOGfGLm2v3uZVdg0TF6c1Tp1vWUXgKkP9UVPRiwZDwfpD1TW9hF4QPbpS4RDqhHCv-LK4q0OVq8lCQb8qgOxXBDG-NC90AWT44_DhDPVVvEFB6eyxc22ETxu555l7COhE0vgwUM0Ojz8T5QslR9cxdKceSbEUqGN41cRPa-Qr_0Z4bqC3VDqWC4QIP_N0d1pkufAbLtConFN37MAmhObtAodmWi9THexZaQ0gO0DXYsDY18lvN_tK4xoCF2bwZCQrE6z8caXZryK_JlSW-5mvxggftW2J1Ya4fMtvDK8YRhbePvsJMplA06RQxsJZDPSOvSAA',
+);
